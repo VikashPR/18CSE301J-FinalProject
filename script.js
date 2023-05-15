c2421b8abfa6bc9b495d0a126d74e1be8ca9e796
Vikash2806 <vikashrajendran2806@gmail.com>
2023-05-16 01:53:04 +0530

    feat: section 3 add

--- script.js
+++ script.js
@@ -6,18 +6,52 @@ const scrollClass = "scroll";
 $(window).on("scroll", () => {
   if (this.matchMedia("(min-width: 992px)").matches) {
     const scrollY = $(this).scrollTop();
-    scrollY > 0
-      ? $body.addClass(scrollClass)
-      : $body.removeClass(scrollClass);
+    scrollY > 0 ? $body.addClass(scrollClass) : $body.removeClass(scrollClass);
   } else {
     $body.removeClass(scrollClass);
   }
 });
 
-$(".page-header .nav-link, .navbar-brand").on("click", function(e) {
+$(".page-header .nav-link, .navbar-brand").on("click", function (e) {
   e.preventDefault();
   const href = $(this).attr("href");
-  $("html, body").animate({
-    scrollTop: $(href).offset().top - 71
-  }, 600);
+  $("html, body").animate(
+    {
+      scrollTop: $(href).offset().top - 71,
+    },
+    600
+  );
 });
+
+var divElement = document.getElementById("viz1684180829227");
+var vizElement = divElement.getElementsByTagName("object")[0];
+vizElement.style.width = "100%";
+vizElement.style.height = divElement.offsetWidth * 0.75 + "px";
+var scriptElement = document.createElement("script");
+scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
+{
+  (" ");
+}
+
+var divElement = document.getElementById("viz1684181164593");
+var vizElement = divElement.getElementsByTagName("object")[0];
+vizElement.style.width = "100%";
+vizElement.style.height = divElement.offsetWidth * 0.75 + "px";
+var scriptElement = document.createElement("script");
+scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
+{
+  (" ");
+}
+
+var divElement = document.getElementById("viz1684181467283");
+var vizElement = divElement.getElementsByTagName("object")[0];
+vizElement.style.width = "100%";
+vizElement.style.height = divElement.offsetWidth * 0.75 + "px";
+var scriptElement = document.createElement("script");
+scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
+{
+  (" ");
+}
